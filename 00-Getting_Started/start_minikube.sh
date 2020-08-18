7d8a8c16a81342f93a143d335c914ad9a383f0f8
Kristin Barkardottir <kristin.barkardottir@netapp.com>
2020-08-18 10:39:32 +0000

    fix script

--- 00-Getting_Started/start_minikube.sh
+++ 00-Getting_Started/start_minikube.sh
@@ -4,14 +4,8 @@ set -e
 
 # Download the executable and the ISO
 echo Downloading minikube components
-if [[ ! -f minikube ]]; then
-  URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube
-  wget $URL
-fi
-if [[ ! -f minikube.iso ]]; then
-  URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube.iso
-  wget $URL
-fi
+[[ ! -f minikube ]] ||  URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube && curl -O $URL ;;
+[[ ! -f minikube.iso ]] || URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube.iso && curl -O $URL ;;
 
 # Verify that the sha256sums are correct
 echo Verifying downloaded components
@@ -19,15 +13,6 @@ sha256sum -c <<EOF
 8f2752174688bc7e0ced3cd63ed26ee51be2207df759f86775928f874defb4e2  minikube
 645c7943a793d30d4d7ea34767a184741f8aece9860a22f9ba2d264825530dfc  minikube.iso
 EOF
-
-echo This script will delete the existing minikube VM if you have one, and
-echo start a new one.
-read -p "Are you sure? [yes/no] " -r
-
-if [[ ! $REPLY =~ ^yes$ ]]; then
-  echo Abort.
-  exit 1
-fi
 
 echo Executing minikube
 chmod +x minikube
