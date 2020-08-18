fe637048415497891f28797c1dae4f02768e40bb
Kristin Barkardottir <kristin.barkardottir@netapp.com>
2020-08-18 11:19:46 +0000

    fix script

--- 00-Getting_Started/start_minikube.sh
+++ 00-Getting_Started/start_minikube.sh
@@ -3,12 +3,12 @@
 set -e
 
 # Download the executable and the ISO
-echo Downloading minikube components
-[[ ! -f minikube ]] || URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube curl -O $URL
-[[ ! -f minikube.iso ]] || URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube.iso curl -O $URL
+echo "Downloading minikube components"
+curl -O https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube
+curl -O  URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube.iso curl
 
 # Verify that the sha256sums are correct
-echo Verifying downloaded components
+echo "Verifying downloaded components"
 sha256sum -c <<EOF
 8f2752174688bc7e0ced3cd63ed26ee51be2207df759f86775928f874defb4e2  minikube
 645c7943a793d30d4d7ea34767a184741f8aece9860a22f9ba2d264825530dfc  minikube.iso
